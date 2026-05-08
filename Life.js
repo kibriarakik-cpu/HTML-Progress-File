201d9bf91c7e4c1211b1e29e191bd139ec345cc3
kibriarakik-cpu <kibriarakik@gmail.com>
2026-05-08 23:25:24 +0600

    Frozen

--- Life.js
+++ Life.js
@@ -1,8 +1,13 @@
 const name = "Life";
 const constant = "Welcome to MySite";
-
-
-
+const add = 5 + 10;
+const sub = 10 - 5;
+const mul = 5 * 10;
+const div = 10 / 5;
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
 
 
 

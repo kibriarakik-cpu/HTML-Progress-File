76811d815aead5d2019f6253f44f9ac848ab27c3
kibriarakik-cpu <kibriarakik@gmail.com>
2026-05-09 22:38:04 +0600

    rowing

--- Life.js
+++ Life.js
@@ -9,8 +9,11 @@ var num4=40;
 function sub(num3, num4) {
     return num3 - num4;
 }
-
-
-
-
-
+var num5=50;
+var num6=60;
+function mul(num5, num6) {
+    return num5 * num6;
+}
+console.log("The sum of num1 and num2 is: " + sum(num1, num2));
+console.log("The difference of num3 and num4 is: " + sub(num3, num4));
+console.log("The product of num5 and num6 is: " + mul(num5, num6)); node

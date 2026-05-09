18dd7357b2e4add260c7b9df349a421e8ce065ab
kibriarakik-cpu <kibriarakik@gmail.com>
2026-05-09 22:26:29 +0600

    stalker

--- Life.js
+++ Life.js
@@ -1,11 +1,16 @@
 var num1=10;
 var num2=20;
-var sum=num1+num2;
-console.log(sum);   
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
 var num3=30;
 var num4=40;
-var sub2=num3-num4;
-console.log(sub2);
+function sub(num3, num4) {
+    return num3 - num4;
+}
+
+
 
 
 

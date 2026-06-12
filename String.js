905e82ebb34e61b21bd176200cd74f698ea61a85
kibriarakik-cpu <kibriarakik@gmail.com>
2026-06-12 21:27:25 +0600

    WC26

--- String.js
+++ String.js
@@ -1,4 +1,22 @@
-const name = "John Doe"; 
-const age = 30;
-
-console.log(`My name is ${name} and I am ${age} years old.`);
+//json-js obj notation
+let object={
+    name:"Messi",
+    age:39,
+    city:"Buenos Aires",
+    country:"Argentina",    
+    GoalScored:940,
+    GOAT:true
+};
+let parsed=JSON.stringify(object);
+console.log(parsed);
+let string =JSON.stringify(object);
+console.log(string);    
+setTimeout(()=>{
+    console.log("Messi's first goal in WC26");
+},20000);
+let Time=setInterval(()=>{
+    console.log("Messi's first goal in WC26");
+},20000);
+setTimeout(()=>{
+    clearInterval(Time);
+},1000);

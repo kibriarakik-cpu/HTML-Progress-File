a3cb4fe6096b0bc43e21ed27384a81eb2ef7d0cd
kibriarakik-cpu <kibriarakik@gmail.com>
2026-07-18 21:56:34 +0600

    MESSI

--- Solve.py
+++ Solve.py
@@ -6,6 +6,14 @@ for i in range( 1, 11):
     print(f"Square of {i} is {i*i}")
     print(f"23 times {i} is {23*i}")
 n=int(input("Enter the number of rows: "))
+num=int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
 for i in range(1, n+1):
     for j in range(1, i+1):
         print(j,"*", end=" ")

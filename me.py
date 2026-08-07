7e3cdb1c553fea84b0a8401509fcd4817c949d7f
kibriarakik-cpu <kibriarakik@gmail.com>
2026-08-07 20:59:54 +0600

    END

--- me.py
+++ me.py
@@ -4,4 +4,14 @@ class Student:
         self.age = age
 grade = "10th"
 print("Hello, my name is", Student("Alice", 15).name, "and I am in", grade, "grade.")
-ob = Student()
+ob = Student("Alice", 15)
+def greet(self):
+    print("Hello, my name is", self.name, "and I am", self.age, "years old.")
+class fruit:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+    def describe(self):
+        print("This fruit is a", self.color, self.name)
+ob.introduction = Student("Alice", 15)
+ob.details= fruit("Apple", "Red")

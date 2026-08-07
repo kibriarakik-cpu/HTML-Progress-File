f80eb6a12672e7bf53c261ce6c9a9aa868c0f957
kibriarakik-cpu <kibriarakik@gmail.com>
2026-08-07 21:12:19 +0600

    END

--- Parrot.py
+++ Parrot.py
@@ -1,6 +1,7 @@
 class Parrot:
     def __init__(self, name):
         self.name = name
+        self.sing = self._sing
 
     def speak(self):
         return f"{self.name} says: Squawk!"
@@ -12,3 +13,11 @@ print("Blue is a{} ".format("blue.species of parrot"))
 print("Woo is also a{} ".format("woo.species of parrot"))
 print("{} is {} years old".format(blue_parrot.name, 5))
 print("{} is {} years old".format(woo_parrot.name, 3))
+def sing(self):
+    return f"{self.name} sings a beautiful song!"
+def sing(self, song):
+    return f"{self.name} sings: {song}"
+print(blue_parrot.sing("Happy Birthday"))
+print(woo_parrot.sing("Twinkle Twinkle Little Star"))
+print("{} dances gracefully.".format(blue_parrot.name))
+print("{} dances gracefully.".format(woo_parrot.name))
